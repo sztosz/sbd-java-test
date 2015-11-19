8c995bde5887d46423a735f8c2496482c82e5909
Bartosz Nowak <sztosz@gmail.com>
2015-11-19 19:16:20 +0100

    one

--- src/main/java/DbTest.java
+++ src/main/java/DbTest.java
@@ -2,6 +2,7 @@ import org.dbunit.Assertion;
 import org.dbunit.DBTestCase;
 import org.dbunit.PropertiesBasedJdbcDatabaseTester;
 import org.dbunit.database.DatabaseConfig;
+import org.dbunit.dataset.DefaultTable;
 import org.dbunit.dataset.IDataSet;
 import org.dbunit.dataset.ITable;
 import org.dbunit.dataset.xml.FlatXmlDataSetBuilder;
@@ -25,25 +26,44 @@ public class DbTest extends DBTestCase {
     }
 
     protected IDataSet getDataSet() throws Exception {
-        return new FlatXmlDataSetBuilder().build(new FileInputStream("dataset.xml"));
+        try {
+            PreparedStatement preparedStatement = getConnection().getConnection().prepareStatement(
+                    "create table TEST_TABLE ( COL0 varchar(255) primary key, COL1 varchar(255), COL2 varchar(255) )");
+            preparedStatement.executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new FlatXmlDataSetBuilder().build(new File("dataset.xml"));
+    }
+//
+    protected DatabaseOperation getSetUpOperation() throws Exception {
+
+        return DatabaseOperation.REFRESH;
     }
 
-//    protected DatabaseOperation getSetUpOperation() throws Exception {
-//        return DatabaseOperation.REFRESH;
-//    }
-//
-//    protected DatabaseOperation getTearDownOperation() throws Exception {
-//        return DatabaseOperation.NONE;
+    protected DatabaseOperation getTearDownOperation() throws Exception {
+        return DatabaseOperation.NONE;
+    }
+
+//    protected void setUp() throws Exception {
+//        super.setUp();
+//        try {
+//            PreparedStatement preparedStatement = getConnection().getConnection().prepareStatement(
+//                    "create table TEST_TABLE ( COL0 varchar(255) primary key,COL1 varchar(255), COL2 varchar(255) )");
+//            preparedStatement.executeUpdate();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
 //    }
 
-//    protected void setUpDatabaseConfig(DatabaseConfig config) {
-//        config.setProperty(DatabaseConfig.PROPERTY_BATCH_SIZE, 97);
-//        config.setFeature(DatabaseConfig.FEATURE_BATCHED_STATEMENTS, true);
-//    }
 
     public void testOne() throws Exception {
         IDataSet databaseDataSet = getConnection().createDataSet();
         ITable actualTable = databaseDataSet.getTable("TEST_TABLE");
+
+        TestedClass testedClass = new TestedClass(getConnection());
+        testedClass.add();
+
 
         // operacje na bazie danych
 
--- src/main/java/TestedClass.java
+++ src/main/java/TestedClass.java
@@ -1,7 +1,10 @@
 import org.dbunit.database.IDatabaseConnection;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 /**
- * Created by sztosz on 11/18/15.
+ * Created by sztosz on 11/18/12.
  */
 public class TestedClass {
     IDatabaseConnection connection;
@@ -9,16 +12,22 @@ public class TestedClass {
         this.connection = connection;
     }
 
-    public void add() {
-
+    public void add() throws SQLException {
+        PreparedStatement preparedStatement = connection.getConnection().prepareStatement(
+                "INSERT INTO TEST_TABLE VALUES ('row 2 col 0', 'row 2 col 1', 'row 2 col 2')");
+        preparedStatement.executeUpdate();
     }
 
-    public void remove() {
-
+    public void remove() throws SQLException {
+        PreparedStatement preparedStatement = connection.getConnection().prepareStatement(
+                "REMOVE FROM TEST_TABLE WHERE COL0='row 0 col 0'");
+        preparedStatement.executeUpdate();
     }
 
-    public void edit() {
-
+    public void edit() throws SQLException {
+        PreparedStatement preparedStatement = connection.getConnection().prepareStatement(
+                "UPDATE TEST_TABLE SET COL0='row 11 col 23', WHERE COL0='row 0 col 0'");
+        preparedStatement.executeUpdate();
     }
 
 }
